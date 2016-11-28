4b1dd9b9efcaefe3c873e857ef3effdf626a7dd6
Yiqin Lu <daaqing452@gmail.com>
2016-11-28 14:59:48 +0800

    login check

--- User/models.py
+++ User/models.py
@@ -3,6 +3,6 @@ from __future__ import unicode_literals
 from django.db import models
 
 class User(models.Model):
+	student_id = models.CharField(max_length=16)
 	name = models.CharField(max_length=16)
-	pswd = models.CharField(max_length=32)
-	student_id = models.CharField(max_lengt=16)
+	password = models.CharField(max_length=32)
--- User/views.py
+++ User/views.py
@@ -3,10 +3,15 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 from django.shortcuts import render_to_response
 from django.template import RequestContext
+from User.models import User
 
 # @csrf_protect
 def login(request):
-	test = "no"
-	if 'username' in request.POST:
-		test = "yes"
-	return render(request, "register.html", {'test':test})
+	info = ""
+	if "student_id" in request.POST:
+		u = User.objects.filter(student_id=request.POST['student_id'])
+		if len(u) != 0:
+			info = "success"
+		else:
+			info = "用户名或密码错误"
+	return render(request, "login.html", {'info':info})
