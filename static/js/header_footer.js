0fc3971e0f4a39270772d5f5d24cf65fe801fdf4
Yiqin Lu <daaqing452@gmail.com>
2016-12-06 00:45:29 +0800

    add header and footer

---
+++ static/js/header_footer.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+	var uid = $('div#header').attr('uid');
+	$('div#header').append('\
+		<a href=\'/logout/\'> 登出 </a> <br/>\
+		<a href=\'/index/\'> 首页 </a> <br/>\
+		<a href=\'/profile/' + uid + '\'> 个人页面 </a> <br/>\
+	');
+});
