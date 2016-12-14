3a41e9de2b04b627db683462cf6c876f7002e81f
Yiqin Lu <daaqing452@gmail.com>
2016-12-14 13:29:27 +0800

    ???

--- Survey/models.py
+++ Survey/models.py
@@ -3,14 +3,28 @@ from __future__ import unicode_literals
 from django.db import models
 
 class Questionaire(models.Model):
+	enable = models.BooleanField(default=1)
+	update_time = models.DateTimeField()
+	title = models.CharField(max_length=64)
+	founder = models.IntegerField()
 	question_list = models.TextField()
+	comment = models.TextField()
 
 class Question(models.Model):
 	qid = models.IntegerField()
+	enable = models.BooleanField(default=1)
 	qtype = models.IntegerField()
 	content = models.TextField()
+	dependency = models.TextField()
+
+class Answeraire(models.Model):
+	qid = models.IntegerField()
+	uid = models.IntegerField()
+	update_time = models.DateTimeField()
+	answer_list = models.TextField()
 
 class Answer(models.Model):
+	aid = models.IntegerField()
 	uid = models.IntegerField()
 	tid = models.IntegerField()
 	answer = models.TextField()
