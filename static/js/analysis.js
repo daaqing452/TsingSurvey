559b9496655487d55ccc7a66982064b81618e1ee
晓晖谢 <xiaohuixie@xiaohuixies-MacBook-Pro.local>
2016-12-25 12:51:35 +0800

    create analysis function

--- static/js/analysis.js
+++ static/js/analysis.js
@@ -1,11 +1,9 @@
 var color = Chart.helpers.color;
-
-$(document).ready(function(){
-	//第一题
-	var barChartData1 = {
-    labels: ["计21班", "计23班", "计24班", "计25班"],
+function drawBar(myID,mytitle,mylabels,mylabel,mydata){
+    var barChartData = {
+    labels: mylabels,
     datasets: [{
-  		label: "人数",
+        label: mylabel,
         backgroundColor:[
         '#ff6384',
         '#ff6384',
@@ -14,13 +12,13 @@ $(document).ready(function(){
         ], 
         borderColor: window.chartColors.white,
         borderWidth: 2,
-        data: [1,2,3,4]
+        data: mydata
     }]
-	};
-	var ctx = document.getElementById("canvas1").getContext("2d");
+    };
+    var ctx = document.getElementById(myID).getContext("2d");
     window.myBar = new Chart(ctx, {
         type: 'bar',
-        data: barChartData1,
+        data: barChartData,
         options: {
             responsive: true,
             legend: {
@@ -28,65 +26,32 @@ $(document).ready(function(){
             },
             title: {
                 display: true,
-                text: '1、你所在的班级是'
+                text: mytitle
             }
         }
     });
 
-    //第二题
-    var barChartData2 = {
-    labels: ["周杰伦", "谢晓晖", "鲁逸沁", "江雨晨"],
+}
+
+function drawDoughnut(myID,mytitle,mylabels,mylabel,mydata){
+    var barChartData = {
+    labels: mylabels,
     datasets: [{
-  		label: "人数",
-        backgroundColor: color(window.chartColors.green).alpha(0.5).rgbString(),
-        borderColor: window.chartColors.green,
+        label: mylabel,
+        backgroundColor:[
+        "#FF6384",
+        "#36A2EB",
+        "#FFCE56"
+        ], 
+        borderColor: window.chartColors.white,
         borderWidth: 2,
-        data: [4,10,3,5]
+        data: mydata
     }]
-
-	};
-    var ctx = document.getElementById("canvas2").getContext("2d");
-    window.myRadar = new Chart(ctx, {
-        type: 'radar',
-        data: barChartData2,
-        options: {
-            responsive: true,
-            legend: {
-                position: 'top',
-            },
-            title: {
-                display: true,
-                text: '2、你最喜欢的电影明星'
-            },
-            scale: {
-              ticks: {
-                beginAtZero: true
-              }
-            }
-        }
-    });
-
-    //第三题
-    var barChartData3 = {
-    labels: ["周杰伦", "谢晓晖", "鲁逸沁", "江雨晨"],
-    datasets: [{
-  		label: "人数",
-        backgroundColor: [
-        	"#FF6384",
-            "#36A2EB",
-            "#FFCE56"
-        	
-        ],
-        borderColor: window.chartColors.green,
-        borderWidth: 2,
-        data: [4,10,3,5]
-    }]
-
-	};
-    var ctx = document.getElementById("canvas3").getContext("2d");
+    };
+    var ctx = document.getElementById(myID).getContext("2d");
     window.myDoughnut = new Chart(ctx, {
         type: 'doughnut',
-        data: barChartData3,
+        data: barChartData,
         options: {
             responsive: true,
             legend: {
@@ -94,10 +59,30 @@ $(document).ready(function(){
             },
             title: {
                 display: true,
-                text: '2、你最喜欢的电影明星'
-            },
-            
+                text: mytitle
+            }
         }
     });
+
+}
+$(document).ready(function(){
+	//第一题
+    var ID = "canvas1"
+    var labels = ["计21班", "计23班", "计24班", "计25班"];
+    var label = "人数";
+    var data = [1,2,3,4];
+    var title = "1、你所在的班级是";
+	drawBar(ID,title,labels,label,data);
+
+    
+
+    //第二题
+    var ID = "canvas2"
+    var labels = ["周杰伦", "谢晓晖", "鲁逸沁", "江雨晨"];
+    var label = "人数";
+    var data = [4,10,3,5];
+    var title = "2、你最喜欢的电影明星";
+    drawDoughnut(ID,title,labels,label,data);
+    
 	
 });
