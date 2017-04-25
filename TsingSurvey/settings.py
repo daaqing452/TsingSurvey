e1a1b83bb7166bfeff7ac46f1ad9e53dc3105435
晓晖谢 <xiaohuixie@xiaohuixies-MacBook-Pro.local>
2017-04-25 23:23:58 +0800

    no message

--- TsingSurvey/settings.py
+++ TsingSurvey/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'SurveyDatabase',
         'USER': 'root',
-        'PASSWORD': 'SurveyDatabase',
+        'PASSWORD': '',
         'HOST': '127.0.0.1',
         'OPTIONS': {'charset': 'utf8mb4'},
     }
