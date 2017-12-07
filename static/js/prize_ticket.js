24ff65fbfe772654923ffd53ce32831830477378
Yiqin Lu <daaqing452@gmail.com>
2017-12-07 16:01:39 +0800

    test

--- static/js/prize_ticket.js
+++ static/js/prize_ticket.js
@@ -5,8 +5,9 @@ $(document).ready(function(){
 function exchange(item) {
 	$('#qrcode').html('')
 	var tid = $(item).attr("tid");
-	var url = window.location.href.replace(window.location.pathname, "") + "/prize_exchange/" + tid + "/";
+	var url = window.location.href;
 	alert(url);
+	url = window.location.href.replace(window.location.pathname, "") + "/prize_exchange/" + tid + "/";
 	$('#qrcode').qrcode(url);
 	$('#main').hide();
 	$.ajax({
