e48d549c223186e978c1f169260d25020de28cb5
晓晖谢 <xiaohuixie@xiaohuixies-MacBook-Pro.local>
2018-06-03 13:31:18 +0800

    bigger images

--- static/js/survey_report.js
+++ static/js/survey_report.js
@@ -651,6 +651,10 @@ function module_select(id){
 				report_status.index ++;
 			}
 		    tableDragable();
+		    var imgs = $("img");
+			for(var i = 0; i < imgs.length; i++){
+				var img = imgs.eq(i).attr("width","60%");
+			}
 			break;
 		}
 	}
